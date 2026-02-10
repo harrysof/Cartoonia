5576e8af6650e81a3a32346f21ebb1ffd643f66f
Sofiane Belkacem Nacer <73562755+harrysof@users.noreply.github.com>
2026-02-10 07:44:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const CONFIG = {
-    YOUTUBE_API_KEY: 'AIzaSyCShN_Jbe6Up4SBR7DX4mQ0ZR6Z6FQBjOc' // Replace with your actual YouTube Data API v3 key
+    YOUTUBE_API_KEY: 'AIzaSyCLRtudy7Chfvvjamep2pj3WzrWZqMtMg0' // Replace with your actual YouTube Data API v3 key
 };
